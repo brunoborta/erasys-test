cb4cca156ebaad0d8c23918d01f82ec83690e3b9
Bruno Bortagaray <khalisto@gmail.com>
2026-02-08 02:08:16 +0100

    feat(next): configure Next.js to allow remote images from hunqz.com

--- apps/web-nextjs/next.config.ts
+++ apps/web-nextjs/next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.hunqz.com',
+        pathname: '/img/usr/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
